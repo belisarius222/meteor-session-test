5c5e9609fa6d62c17ba853af7c377e355f00469c
Ted Blackman <ted.blackman@gmail.com>
2013-01-18 02:10:51 -0800

    print types for both arguments

--- sessionTest.js
+++ sessionTest.js
@@ -1,6 +1,6 @@
 if (Meteor.isClient) {
   Handlebars.registerHelper('sessionEquals',function(first, second){
-    console.log(typeof first);
+    console.log((typeof first)+" "+(typeof second));
     return Session.equals(first,second);
   });
 
